011591d3172a7d046ce98bfc76a9bd72ccc11a12
Aathish1 <iamtheonlyalpha@gmail.com>
2025-04-24 12:14:35 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import "./css/App.css";
+import Favorites from "./pages/Favorites";
+import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
+import { MovieProvider } from "./contexts/MovieContext";
+import NavBar from "./components/NavBar";
+import Watchlist from "./pages/Watchlist";
+
+function App() {
+  return (
+    <MovieProvider>
+      <NavBar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+          <Route path="/watchlist" element={<Watchlist />} />
+        </Routes>
+      </main>
+    </MovieProvider>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom";
+import "../css/Navbar.css";
+
+function NavBar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-brand">
+        <Link to="/">MovZov</Link>
+      </div>
+      <div className="navbar-links">
+        <Link to="/" className="nav-link">
+          Home
+        </Link>
+        <Link to="/favorites" className="nav-link">
+          Favorites
+        </Link>
+        {/* <Link to="/watchlist" className="nav-link">
+          Watchlist
+        </Link> */}
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;
